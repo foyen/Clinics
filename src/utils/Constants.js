826ef33ebc22eac18a59165c99d430dde669686d
Francis <fioyeniy@asu.edu>
2019-05-27 21:38:13 -0700

    Global constants file

---
+++ src/utils/Constants.js
@@ -0,0 +1,11 @@
+export const CONSTANTS  = {
+    scriptUrl:"https://maps.googleapis.com/maps/api/js",
+    apiKey: "",
+     
+    phoenixCoordinates:{
+        lat: 33.4484,
+        lng: -112.0740
+    }
+}
+
+export default CONSTANTS
